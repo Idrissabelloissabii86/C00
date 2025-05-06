661b0a2890ddbec0bfdec164b316078aacaa2f9a
BELLO ISSA IDRISSA <64420230+Idrissabelloissabii86@users.noreply.github.com>
2025-05-06 15:08:09 +0200

    Update current_datetime.sh

--- current_datetime.sh
+++ current_datetime.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# ceci affiche la date du sytème sous le format YYYY-MM-DD HH-MM-SS
-date + "%Y-%m-%d %H:%M:%S"
+# ceci affiche la date du sytème sous le format YYYY-MM-DD HH:MM:SS
+date + "%YYYY-%mm-%dd %HH:%MM:%SS"
