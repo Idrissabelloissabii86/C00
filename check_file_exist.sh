647ec029176c75676c67e72c7f58d35b19060f8a
Idrissabelloissabii86 <idrissabelloissa.bii86@gmail.com>
2025-05-06 13:45:40 +0200

    Premier Commit

---
+++ check_file_exist.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Ce script permet de vérifier si un fichier donné existe déjà ou pas
+
+# recuperation du nom du fichier et stockage de ce dernier dans la variable $ichier
+read -p "Entrez le nom du fichier : " fichier
+# la condition de vérification si le fichier existe ou pas en retournant un message
+if [ -f $fichier ]; then
+echo "Le fichier existe"
+else
+echo "le fichier n'existe pas"
+fi
---
+++ hello_world.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Ce script affiche le message (Hello, World!) lorsqu'on l'exécute
+echo "Hello, World!"
