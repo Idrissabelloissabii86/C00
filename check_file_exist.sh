595cabbcac622b0bb96ad11c8e6e2c1080441142
BELLO ISSA IDRISSA <64420230+Idrissabelloissabii86@users.noreply.github.com>
2025-05-06 17:52:21 +0200

    Update check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -4,7 +4,6 @@
 # recuperation du nom du fichier et stockage de ce dernier dans la variable $ichier
 read -p "Entrez le nom du fichier : " fichier
 # la condition de vérification si le fichier existe ou pas en retournant un message
-# Vérifier si le fichier existe
 if [ -f "$fichier" ]; then
     echo "Le fichier '$fichier' existe"
 fi
