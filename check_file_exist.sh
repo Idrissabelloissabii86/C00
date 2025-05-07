6f5e3df75a296f9dcd5dd124db4866fdeb31b385
BELLO ISSA IDRISSA <64420230+Idrissabelloissabii86@users.noreply.github.com>
2025-05-07 02:31:42 +0200

    Update check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -2,7 +2,7 @@
 # Ce script permet de vérifier si un fichier donné existe déjà ou pas
 echo "Entrez le nom du fichier :"
 read fichier
-if [ -f "$fichier" ] then
+if [-f "$fichier"]; then
   echo "Le fichier '$fichier' existe"
 else
   echo "Le fichier '$fichier' n'existe pas"
